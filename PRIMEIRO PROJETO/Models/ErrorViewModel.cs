58eede0c441865f3fd3d6182a52736deefb81592
juliano.pereira1 <juliano.pereira1@BRE203D16>
2023-07-26 21:19:55 -0300

    Adicionar arquivos de projeto.

---
+++ PRIMEIRO PROJETO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRIMEIRO_PROJETO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
