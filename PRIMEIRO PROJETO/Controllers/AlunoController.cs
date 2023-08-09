57337d6c18b8bb6758f914898eda754d0bccb881
JULIANO PEREIRA <juliano.pereira1@alunos.sc.senac.br>
2023-08-09 19:36:41 -0300

    Alterado na aula 19:36

---
+++ PRIMEIRO PROJETO/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRIMEIRO_PROJETO.Controllers
+{
+    public class Aluno : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
